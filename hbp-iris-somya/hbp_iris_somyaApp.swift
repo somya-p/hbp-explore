92633bd3301dbbd813013edaf30c043aab55b9c2
Somya Prabhakar <prabhakar.somya@gmail.com>
2023-02-10 23:15:48 -0500

    initial commit

---
+++ hbp-iris-somya/hbp_iris_somyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hbp_iris_somyaApp.swift
+//  hbp-iris-somya
+//
+//  Created by Somya Prabhakar on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct hbp_iris_somyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
